9a0544f8a245b386e2e689953a1c764c747562a5
Khawar Anees <khawaraneescp@gmail.com>
2023-11-30 15:34:58 +0500

    more faqs points

--- assets/about-faqs.js
+++ assets/about-faqs.js
@@ -1,25 +1,46 @@
 var acc = document.getElementsByClassName("accordion-faqs");
-var i;
 
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("mouseenter", function() {
-    this.classList.add("accordion-active");
-    var panel = this.nextElementSibling;
-    if (!panel.style.maxHeight) {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+function openAccordion() {
+  var panel = this.nextElementSibling;
+  this.classList.add("accordion-active");
+  panel.style.maxHeight = panel.scrollHeight + "px";
 }
 
+function closeAccordion() {
+  var panel = this.nextElementSibling;
+  this.classList.remove("accordion-active");
+  panel.style.maxHeight = null;
+}
 
-var acc_item = document.getElementsByClassName("item-faq-wrapper");
-for (let s = 0; s < acc_item.length; s++) {
-  acc_item[s].addEventListener("mouseleave", function() {
-    this.querySelector('.accordion-faqs').classList.remove("accordion-active");
-    var panel = this.querySelector('.accordion-faqs').nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null
-    }
+function toggleAccordion() {
+  var isActive = this.classList.contains("accordion-active");
+
+  // Close all accordions
+  for (let i = 0; i < acc.length; i++) {
+    var otherPanel = acc[i].nextElementSibling;
+    acc[i].classList.remove("accordion-active");
+    otherPanel.style.maxHeight = null;
+  }
+
+  // Open the clicked accordion if it was not active
+  if (!isActive) {
+    openAccordion.call(this);
+  }
+}
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("mouseenter", openAccordion);
+  acc[i].addEventListener("mouseleave", closeAccordion);
+
+  // Remove the click event listener if present to avoid double click issues
+  acc[i].removeEventListener("click", toggleAccordion);
+
+  // Add click event listener for mobile devices
+  acc[i].addEventListener("click", toggleAccordion);
+
+  // Prevent the default behavior of the click event on mobile devices
+  acc[i].addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    toggleAccordion.call(this);
   });
 }
